8e52cf6f04d0095baaba55f54175d3f9b8044d7b
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-15 11:03:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-3xl text-red-500 bg-red-200'>Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
